d514a0d1991e32ac923041190aef18d472f0cce7
Anastasia <asya_apto@mail.ru>
2021-11-29 14:38:31 +0300

    Optimize statements with None and 0

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from terminaltables import SingleTable
 
 
 def predict_salary(salary_from, salary_to):
-    if not salary_to or salary_to == 0:
+    if not salary_to:
         average_salary = salary_from * 1.2
-    elif not salary_from or salary_from == 0:
+    elif not salary_from:
         average_salary = salary_to * 0.8
     else:
         average_salary = mean([salary_from, salary_to])
@@ -71,7 +71,7 @@ def handle_hh_vacancies(languages):
 
         all_salaries = [vacancy["salary"] for vacancy in all_vacancies]
         processed_vacancies_salary = [predicted_salary for salary in all_salaries
-                                      if (predicted_salary := predict_rub_salary_for_hh(salary)) is not None]
+                                      if (predicted_salary := predict_rub_salary_for_hh(salary))]
 
         average_salary_by_lang[lang] = {
             "vacancies_found": page_vacancies["found"],
